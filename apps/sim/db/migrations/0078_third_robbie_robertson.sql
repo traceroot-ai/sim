82e85c72325f077ea2dd643bc02090090285792f
Vikhyath Mondreti <vikhyath@simstudio.ai>
2025-08-26 15:17:49 -0700

    Remove 78th migration to prepare for merge with staging

--- apps/sim/db/migrations/0078_third_robbie_robertson.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE "subscription" DROP CONSTRAINT "check_enterprise_metadata";--> statement-breakpoint
-ALTER TABLE "organization" ADD COLUMN "org_usage_limit" numeric;--> statement-breakpoint
-ALTER TABLE "user_stats" DROP COLUMN "usage_limit_set_by";--> statement-breakpoint
-ALTER TABLE "user_stats" DROP COLUMN "billing_period_start";--> statement-breakpoint
-ALTER TABLE "user_stats" DROP COLUMN "billing_period_end";--> statement-breakpoint
-ALTER TABLE "subscription" ADD CONSTRAINT "check_enterprise_metadata" CHECK (plan != 'enterprise' OR metadata IS NOT NULL);
