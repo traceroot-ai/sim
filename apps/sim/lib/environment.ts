a70ea65671bf9209e545651df7d5174db80e2263
Vikhyath Mondreti <vikhyath@simstudio.ai>
2025-08-26 17:55:38 -0700

    restore environment ts file

--- apps/sim/lib/environment.ts
+++ apps/sim/lib/environment.ts
@@ -6,17 +6,17 @@ import { env, isTruthy } from './env'
 /**
  * Is the application running in production mode
  */
-export const isProd = true
+export const isProd = env.NODE_ENV === 'production'
 
 /**
  * Is the application running in development mode
  */
-export const isDev = false
+export const isDev = env.NODE_ENV === 'development'
 
 /**
  * Is the application running in test mode
  */
-export const isTest = false
+export const isTest = env.NODE_ENV === 'test'
 
 /**
  * Is this the hosted version of the application
